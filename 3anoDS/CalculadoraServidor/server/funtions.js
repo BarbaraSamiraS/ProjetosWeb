2f664ccab7b7e8e0bcc974161ab07180e9d514e6
Barbara  Samira Santiesteban Saravia <145080442+BarbaraSamiraS@users.noreply.github.com>
2025-03-22 21:06:50 -0300

    Add files via upload

---
+++ 3anoDS/CalculadoraServidor/server/funtions.js
@@ -0,0 +1,33 @@
+function ad(variavel, digitos) {
+    return variavel + digitos;
+} 
+
+function zero(variavel) {
+    return variavel + "0";
+}
+
+function apagarTudo() {
+    return "";
+}
+
+function del(variavel) {
+    if (variavel.length > 0) {
+        variavel = variavel.slice(0, -1);
+    }
+    return variavel;
+}
+
+function resul( variavel) {
+    if (variavel === "0/0") {
+        return "Error 0";
+    } else {
+        try{
+            return eval(variavel);
+        } catch {
+            return "Error com a conta";
+        }
+    }
+    
+}
+
+module.exports = {ad, zero, apagarTudo, del, resul}
